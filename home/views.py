6777837b6e13166d80df302e997854446359b6b5
tharubijay <bijaytharu306@gmail.com>
2022-11-08 08:52:20 +0545

    first commit

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+def price(request):
+    return render(request,'price.html')
+
+def services(request):
+    return render(request,'services.html')
